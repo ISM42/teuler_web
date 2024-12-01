017c4e33365ca1f029e24e7cc53b4ccf20aaf891
Estrella1709 <estrellatorrijos17@gmail.com>
2024-12-01 01:09:46 -0600

    Modal eliminar cuenta - Aplicación de estilos

--- teuler/resources/views/perfil_usuario.blade.php
+++ teuler/resources/views/perfil_usuario.blade.php
@@ -169,6 +169,7 @@
                 <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
+                <h1 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">¡Espera!</h1>
                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">¿Estás seguro que deseas eliminar tu cuenta? Esta acción no se podrá deshacer</h3>
 
                 <form action="{{ route('usuario.eliminar.cuenta') }}" method="POST">
@@ -176,7 +177,7 @@
                     <button type="submit" data-modal-hide="popup-modal" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                         Sí, continuar
                     </button>
-                    <button type="button" data-modal-hide="popup-modal" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    <button type="button" data-modal-hide="popup-modal" class="buttonC py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-white-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                         No, cancelar
                     </button>
                 </form>
