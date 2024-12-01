0c1593cfed4c473d63d26c3d1dbe3e0fc921f161
Estrella1709 <estrellatorrijos17@gmail.com>
2024-11-30 19:20:27 -0600

    Vista Login - Aplicación de estilos

--- teuler/resources/views/index_teuler.blade.php
+++ teuler/resources/views/index_teuler.blade.php
@@ -2,39 +2,46 @@
 @section('titulo','Inicio')
 @section('contenido')
 
-<h1 class="text-gray-900 font-bold font-sans text-3xl mt-5 mb-5 flex justify-center"> TEULER </h1>
 
-<div class="container flex justify-center items-center min-h-screen mb-10 mt-10 pb-36 ">
-    <img src="{{ asset('images/logo_teuler.png') }}" alt="" width="250" height="250">
-  
+<link rel="stylesheet" href="{{ asset('css/index_teuler.css') }}">
+
+<main>
+    <div class="left-panel">
+        <div class="card">
+        <h1 class="text-4xl font-bold text-gray-800 mb-6">Inicio Sesión</h1>
+            <div class="container mb-10 pb-10">
+                <form class="max-w-sm mx-auto" action="/login" method= "POST">
+                    @csrf
+                    <div class="mb-5">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="email" name="email" id="email" class="rounded-lg w-full p-2.5" required />
+                    </div>
+                    <div class="mb-5">
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+                        <input type="password" id="password" name="contraseña" class="rounded-lg w-full p-2.5" required />
+                    </div>
+                    <div class="flex items-start mb-5">
+                    <!--  <a href="/home" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">¿Eres nuevo? Regístrate</a> -->
+                    <!-- Modal toggle -->
+                    <button data-modal-target="registro_usuario" data-modal-toggle="registro_usuario" class="buttonRegistro ms-2 text-sm underline font-medium text-gray-900 dark:text-gray-300" type="button">
+                    ¿Eres nuevo? Regístrate
+                    </button>
+
+                    <button type="submit" class="buttonI text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar</button>
+                    </div>                
+                </form>
+            <div>
+        </div>
+    </div>
+</main>
+
+<div class="right-panel">
+    <div class="container flex flex-col justify-center items-center min-h-screen mb-10 mt-10 pb-36">
+    <h1 class="text-gray-900 font-bold font-sans text-3xl mt-5 mb-5 flex justify-center"> TEULER </h1>
+        <img src="{{ asset('images/logo_teuler.png') }}" alt="" width="350" height="350">
+    
+    </div>
 </div>
-
-
-<div class="container mb-10 pb-10">
-<form class="max-w-sm mx-auto" action="/login" method= "POST">
-    @csrf
-  <div class="mb-5">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-  </div>
-  <div class="mb-5">
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
-    <input type="password" id="password" name="contraseña" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-  </div>
-  <div class="flex items-start mb-5">
-
-   <!--  <a href="/home" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">¿Eres nuevo? Regístrate</a> -->
-
-   <!-- Modal toggle -->
-<button data-modal-target="registro_usuario" data-modal-toggle="registro_usuario" class="ms-2 text-sm underline font-medium text-gray-900 dark:text-gray-300" type="button">
-¿Eres nuevo? Regístrate
-</button>
-  </div>
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar</button>
-</form>
-<div>
-
-
 
 <!-- Main modal REGISTRO-->
 <div id="registro_usuario" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
